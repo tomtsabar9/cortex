b4d066922f107b913e76d03ee4497e0484695635
Tom Tsabar <tomtsabar9@gmail.com>
2020-05-30 23:03:05 +0300

    cli more

--- cortex/cli/__main__.py
+++ cortex/cli/__main__.py
@@ -5,23 +5,24 @@ import struct
 import click
 import requests 
 
-URL = "http://127.0.0.1:5000/"
+URL = 'http://<ip>:<port>/'
 
 @click.group()
-@click.option('--host', default="127.0.0.1", help='server\'s ip')
-@click.option('--port', default=8000, help='server\'s port')
+@click.option('-h', '--host', default="127.0.0.1", help='server\'s ip')
+@click.option('-p', '--port', default=5000, help='server\'s port')
 def reflect(host, port):
     """
     Just a grouping for other commands
     """
-    pass
+    global URL
+    URL = URL.replace('<ip>', host).replace('<port>', str(port))
    
 @reflect.command()
 def get_users():
     
     users_url = URL + "users"
     # sending get request and saving the response as response object 
-    r = requests.get(url = users_url) 
+    r = requests.get(url = users_url, timeout=1) 
   
     # extracting data in json format 
     data = r.json() 
@@ -30,9 +31,9 @@ def get_users():
 @reflect.command()
 @click.argument('user-id')
 def get_user(user_id):
-    user_url = URL + "users/" + user_id
+    user_url = URL + 'users/' + user_id
     # sending get request and saving the response as response object 
-    r = requests.get(url = user_url) 
+    r = requests.get(url = user_url, timeout=1) 
   
     # extracting data in json format 
     data = r.json() 
@@ -41,9 +42,9 @@ def get_user(user_id):
 @reflect.command()
 @click.argument('user-id')
 def get_snapshots(user_id):
-    snapshots_url = URL + "users/" + user_id + "/snapshots"
+    snapshots_url = URL + 'users/' + user_id + '/snapshots'
     # sending get request and saving the response as response object 
-    r = requests.get(url = snapshots_url) 
+    r = requests.get(url = snapshots_url, timeout=1) 
   
     # extracting data in json format 
     data = r.json() 
@@ -55,7 +56,7 @@ def get_snapshots(user_id):
 def get_snapshot(user_id, snapshot_id):
     snapshot_url = URL + "users/" + user_id + "/snapshots/" + snapshot_id
     # sending get request and saving the response as response object 
-    r = requests.get(url = snapshot_url) 
+    r = requests.get(url = snapshot_url, timeout=1) 
   
     # extracting data in json format 
     data = r.json() 
@@ -68,7 +69,7 @@ def get_snapshot(user_id, snapshot_id):
 def get_result(user_id, snapshot_id, result):
     result_url = URL + "users/" + user_id + "/snapshots/" + snapshot_id + "/" +result
     # sending get request and saving the response as response object 
-    r = requests.get(url = result_url) 
+    r = requests.get(url = result_url, timeout=1) 
     
     # extracting data in json format 
     data = r.text 
